01c6e4db6b22d44548656bfcbac439bfbaba0301
Adrian Araujo <adrianaraujo247@gmail.com>
2022-09-10 10:43:27 -0300

    :recycle: refactor: alterando estilo do botão de carrinho

--- src/components/FloatButton/index.tsx
+++ src/components/FloatButton/index.tsx
@@ -10,10 +10,22 @@ function FloatButton () {
   const { cart } = useCart()
   const navigate = useNavigate()
 
+  const subTotal = () => {
+    let total = 0
+    cart.forEach((item) => {
+      total += item.amount * item.price
+    })
+    return total
+  }
+
   return (
     <Button onClick={() => navigate('/carrinho')}>
-      <span>{cart.length}</span>
-      <IoCartSharp size={30} color="#fff"/>
+      <div>
+        <strong>{cart.length}</strong>
+        <IoCartSharp size={30} color="#fff"/>
+      </div>
+      <span>ver carrinho</span>
+      <span>{`R$ ${subTotal().toFixed(2)}`}</span>
     </Button>
   )
 }
--- src/components/FloatButton/styles.ts
+++ src/components/FloatButton/styles.ts
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 10px;
   background-color: ${props => props.theme.colors.main};
   border: none;
-  border-radius: 30px;
-  width: 50px;
-  height: 50px;
+  width: 100%;
+  height: 60px;
   position: fixed;
-  right: 10px;
-  bottom: 15px;
+  right: 0px;
+  bottom: 0px;
 
   box-shadow: 2px 2px 10px ${props => props.theme.colors.palette.zinc[400]};
   transition: 0.3s;
@@ -19,20 +21,25 @@ export const Button = styled.button`
     cursor: pointer;
   }
 
-  span {
+  strong {
     background-color: red;
-    width: 23px;
-    height: 23px;
+    width: 18px;
+    height: 18px;
     border-radius: 15px;
     position: absolute;
-    margin-top: -15px;
+    margin-top: -5px;
     margin-left: 20px;
     color: #fff;
     font-weight: bold;
-    font-size: 14px;
+    font-size: 12px;
     display: flex;
     justify-content: center;
     align-items: center;
     padding: 5px;
+  }
+
+  span {
+    font-size: 18px;
+    color: #fff;
   }
 `
